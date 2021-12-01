1530056a9dcd8eb46621f0189bfdd2a24a39590d
Yunus Akgeç <yunusakgec@gmail.com>
2021-12-01 15:39:18 +0300

    Değisiklikler yapildi.

---
+++ src/KdvTutar.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class KdvTutar {
+    public static void main(String[] args) {
+        double tutar , kucukKdvOran = 0.18 , buyukKdvOran = 0.08;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ücret tutarını giriniz:");
+        tutar= input.nextDouble();
+        if ( tutar > 0){
+            if (tutar < 1000) {
+                double kdvTutar = tutar * kucukKdvOran;
+                double kdvliTutar = tutar + kdvTutar ;
+                System.out.println("KDV'siz Tutar: " + tutar);
+                System.out.println("KDV Oranı : " + kucukKdvOran);
+                System.out.println("KDV Tutarı : " +kdvTutar);
+                System.out.println("KDV'li Tutar : " + kdvliTutar);
+
+            }
+        }
+        if ( tutar > 1000) {
+            double kdvTutar = tutar * buyukKdvOran ;
+            double kdvliTutar = tutar + kdvTutar ;
+            System.out.println("KDV'siz Tutar: " + tutar);
+            System.out.println("KDV Oranı : " + buyukKdvOran);
+            System.out.println("KDV Tutarı : " +kdvTutar);
+            System.out.println("KDV'li Tutar : " + kdvliTutar);
+        }
+    }
+}
