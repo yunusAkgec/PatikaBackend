152f3847646548644b61a49299f88e9507a7d46c
Yunus Akgeç <yunusakgec@gmail.com>
2021-12-10 01:41:56 +0300

    Değisiklikler yapildi.

--- src/salaryCalculator/Employee.java
+++ src/salaryCalculator/Employee.java
@@ -17,22 +17,21 @@ public class Employee {
         this.salary = salary;
         this.workHours = workHours;
         this.hireYear = hireYear;
+        print();
     }
 
     public double tax(){
         if (this.salary >= 1000) {
-            double duty = this.salary*0.03;
-
+            return this.salary*0.03;
         }
         else{
-            return salary =1000;
+            return 0;
         }
-        return 0;
     }
-    public void bonus(){
+    public double bonus(){
         if (workHours < 168 && workHours > 0) {
             if (this.workHours > 40) {
-                double bonus = (double) ((this.workHours - 40) * 30);
+                return ((this.workHours - 40) * 30);
             } else {
                 bonus = 0;
             }
@@ -40,32 +39,37 @@ public class Employee {
         else {
             System.out.println("Hatalı Veri Girişi Yaptınız.");
         }
+        return 0;
     }
-    public void raiseSalary(){
+    public double rise(){
         if ( this.hireYear < 10 ){
-            this.rise= (this.salary*0.05) ;
+            return (this.salary * 0.05) ;
         }
-        if (this.hireYear>9 && this.hireYear<20){
-            this.rise= (this.salary*0.1);
+        if ( this.hireYear > 9 && this.hireYear<20){
+            return (this.salary*0.1);
         }
         if (this.hireYear>19 ) {
-            this.rise=(this.salary*0.05);
+            return (this.salary*0.15);
         }
+        return 0;
     }
-    public void newSalary(){
-        double newSalary = this.salary + this.bonus - this.tax();
+    public double newSalary(){
+        return (this.salary + this.bonus() - this.tax());
+
     }
 
     public void print(){
+        System.out.println("-------------------------");
         System.out.println("Adı : " + this.name );
         System.out.println("Maaşı : " + this.salary);
         System.out.println("Çalışma Saati :" + this.workHours);
         System.out.println("Başlangıç Yılı : " + this.hireYear);
-        System.out.println("Vergi : " + this.tax );
-        System.out.println("Bonus : " + this.bonus);
-        System.out.println("Maaş Artışı : " + this.rise);
-        System.out.println("Vergiler ve Bonuslarla Beraber Aldığı Maaş : " + this.newSalary);
-        System.out.println("Toplam Maaş : " + (this.newSalary + this.rise));
+        System.out.println("Vergi : " + this.tax() );
+        System.out.println("Bonus : " + this.bonus());
+        System.out.println("Maaş Artışı : " + this.rise());
+        System.out.println("Vergiler ve Bonuslarla Beraber Aldığı Maaş : " + this.newSalary());
+        System.out.println("Toplam Maaş : " + (this.newSalary() + this.rise()));
+        System.out.println("-----------------------------------");
 
     }
 }
